0a89c145183f182ce6a0bef0c3bf3b1c610af692
WillianPer <wsresilienttasteshopw@gmail.com>
2022-09-26 23:35:13 -0400

    Produto Service!!

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,4 +1,6 @@
+import { ProductService } from './../../product/product.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-create',
@@ -7,15 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCreateComponent implements OnInit {
 
-  propLegal = "qualquer"
+  // propLegal = "qualquer"
 
-  constructor() { }
+  constructor(private productService: ProductService, private router: Router) { }
 
   ngOnInit(): void {
+    // this.productService.showOnConsole('teste...')
+    // this.productService.showMessage('Operação executada com sucesso!!')
   }
 
-  fazerAlgo(): void {
-    console.log('Fazendo Algo!!')
+  
+  // fazerAlgo(): void {
+    //   console.log('Fazendo Algo!!')
+    // }
+    
+  createProduct(): void {
+    this.productService.showMessage('Produto Criado!!')
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products'])
   }
 
 }
