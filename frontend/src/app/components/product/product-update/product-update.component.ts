13b133563d0a29094597b015b3d8eebab5e590fd
WillianPer <wsresilienttasteshopw@gmail.com>
2022-10-08 23:08:28 -0400

    Component Atualizar Produto #2!!

--- frontend/src/app/components/product/product-update/product-update.component.ts
+++ frontend/src/app/components/product/product-update/product-update.component.ts
@@ -1,3 +1,6 @@
+import { Product } from './../product.model';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductUpdateComponent implements OnInit {
 
-  constructor() { }
+  constructor(private productService: ProductService, private router: Router, private route: ActivatedRoute) { }
+
+  product!: Product;
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id')
+    this.productService.readById(id!).subscribe(product => {
+      this.product = product
+    })
+  }
+
+  updateProduct(): void {
+    this.productService.update(this.product).subscribe(() => {
+      this.productService.showMessage('Produto alterado com sucesso!!')
+      this.router.navigate(['/products'])
+    })
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products'])
   }
 
 }
